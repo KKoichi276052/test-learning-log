eedafe4861c2b31ebc5a301d3a1bb71273e2ea53
Koichi Kataoka <k.koichi276052@gmail.com>
2025-02-13 16:13:16 +0900

    update counter component

--- rtl-storybook/src/components/Counter.tsx
+++ rtl-storybook/src/components/Counter.tsx
@@ -10,22 +10,22 @@
 import { useState } from "react";
 
 interface CounterProps {
-  initialCount?: number;
+	initialCount?: number;
 }
 
 const Counter: React.FC<CounterProps> = ({ initialCount = 0 }) => {
-  const [count, setCount] = useState(initialCount);
+	const [count, setCount] = useState(initialCount);
 
-  const increment = () => setCount(count + 1);
-  const decrement = () => setCount(count - 1);
+	const increment = () => setCount(count + 1);
+	const decrement = () => setCount(count - 1);
 
-  return (
-    <div>
-      <h2>Count: {count}</h2>
-      <button onClick={decrement}>ー</button>
-      <button onClick={increment}>＋</button>
-    </div>
-  );
+	return (
+		<div>
+			<h2>Counter: {count}</h2>
+			<button onClick={decrement}>ー</button>
+			<button onClick={increment}>＋</button>
+		</div>
+	);
 };
 
 export default Counter;
